936165a45ec38adba62feb31b3c2d9dba3b5f48a
Esteban Echeverry <eecheverry@nubark.com>
2020-05-08 17:27:40 -0500

    Create curses window inside widgets

--- tests/widget/test_widget.py
+++ tests/widget/test_widget.py
@@ -1,4 +1,18 @@
+import curses
+from pytest import fixture
 from widark.widget import Widget
+
+
+@fixture
+def stdscr():
+    stdscr = None
+
+    def _init(screen):
+        nonlocal stdscr
+        stdscr = screen
+
+    curses.wrapper(_init)
+    return stdscr
 
 
 def test_widget_instantiation_defaults():
@@ -6,3 +20,17 @@ def test_widget_instantiation_defaults():
     assert isinstance(widget, Widget)
     assert widget.parent is None
     assert widget.children == []
+
+
+def test_widget_window(stdscr):
+    root = Widget(None)
+    root.window = stdscr
+
+    widget = Widget(root)
+
+    relative_coordinates = widget.window.getparyx()
+    dimensions = widget.window.getmaxyx()
+
+    assert widget.window is not None
+    assert relative_coordinates == (0, 0)
+    assert dimensions == (1, 1)
--- widark/widget/widget.py
+++ widark/widget/widget.py
@@ -5,3 +5,8 @@ class Widget:
     def __init__(self, parent: Optional['Widget']) -> None:
         self.parent: Optional['Widget'] = parent
         self.children: List['Widget'] = []
+        self.window = None
+
+        if self.parent:
+            factory = parent.window.derwin  # type: ignore
+            self.window = factory(1, 1, 0, 0)
