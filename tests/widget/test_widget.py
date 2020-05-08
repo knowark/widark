27f2e50601998c5844b035eba51d3262c1ca359f
Esteban Echeverry <eecheverry@nubark.com>
2020-05-08 16:29:28 -0500

    Make parent parameter mandatory in widget

--- tests/widget/test_widget.py
+++ tests/widget/test_widget.py
@@ -2,7 +2,7 @@ from widark.widget import Widget
 
 
 def test_widget_instantiation_defaults():
-    widget = Widget()
+    widget = Widget(None)
     assert isinstance(widget, Widget)
     assert widget.parent is None
     assert widget.children == []
--- widark/widget/widget.py
+++ widark/widget/widget.py
@@ -2,6 +2,6 @@ from typing import List, Optional
 
 
 class Widget:
-    def __init__(self, parent: 'Widget' = None) -> None:
+    def __init__(self, parent: Optional['Widget']) -> None:
         self.parent: Optional['Widget'] = parent
         self.children: List['Widget'] = []
