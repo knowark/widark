03e201db2906f2a4b2e9ead341de443416c77306
Esteban Echeverry <eecheverry@nubark.com>
2020-05-09 10:54:19 -0500

    Define sequence attributes and methods in widget

--- tests/widget/test_widget.py
+++ tests/widget/test_widget.py
@@ -15,22 +15,38 @@ def stdscr():
     return stdscr
 
 
+@fixture
+def root(stdscr):
+    root = Widget(None)
+    root.window = stdscr
+    return root
+
+
 def test_widget_instantiation_defaults():
     widget = Widget(None)
     assert isinstance(widget, Widget)
     assert widget.parent is None
     assert widget.children == []
+    assert widget.content == ''
+    assert widget.row_sequence == 0
+    assert widget.column_sequence == 0
 
 
-def test_widget_window(stdscr):
-    root = Widget(None)
-    root.window = stdscr
-
+def test_widget_window(root):
     widget = Widget(root)
 
     relative_coordinates = widget.window.getparyx()
     dimensions = widget.window.getmaxyx()
 
+    assert widget in root.children
     assert widget.window is not None
     assert relative_coordinates == (0, 0)
     assert dimensions == (1, 1)
+
+
+def test_widget_position(root):
+    widget = Widget(root).sequence(row=1, column=2)
+
+    assert isinstance(widget, Widget)
+    assert widget.row_sequence == 1
+    assert widget.column_sequence == 2
--- widark/widget/widget.py
+++ widark/widget/widget.py
@@ -6,7 +6,16 @@ class Widget:
         self.parent: Optional['Widget'] = parent
         self.children: List['Widget'] = []
         self.window = None
+        self.content = ''
+        self.row_sequence = 0
+        self.column_sequence = 0
 
         if self.parent:
+            self.parent.children.append(self)
             factory = parent.window.derwin  # type: ignore
             self.window = factory(1, 1, 0, 0)
+
+    def sequence(self, row=0, column=0) -> 'Widget':
+        self.row_sequence = row
+        self.column_sequence = column
+        return self
