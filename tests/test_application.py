91e68b26c17c771ac52cdbece3f2f017b53a290c
Esteban Echeverry <eecheverry@nubark.com>
2020-05-08 19:17:21 -0500

    Add application run coroutine

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,8 +1,20 @@
+import pytest
 from widark.application import Application
 from widark.widget import Widget
 
 
+pytestmark = pytest.mark.asyncio
+
+
 def test_application_instantiation():
     application = Application()
+
     assert isinstance(application, Widget)
+    assert application.window is None
+
+
+async def test_application_run():
+    application = Application()
+    await application.run()
+
     assert application.window is not None
--- widark/application.py
+++ widark/application.py
@@ -5,7 +5,9 @@ from .widget import Widget
 class Application(Widget):
     def __init__(self) -> None:
         super().__init__(None)
-        curses.wrapper(self._setup_screen)
 
     def _setup_screen(self, stdscr) -> None:
         self.window = stdscr
+
+    async def run(self) -> None:
+        curses.wrapper(self._setup_screen)
