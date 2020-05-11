1c56d0b3b6283f27f9c3c7309896e0f2aa7b1560
Esteban Echeverry <eecheverry@nubark.com>
2020-05-10 22:10:01 -0500

    Update showcase

--- showcase/__main__.py
+++ showcase/__main__.py
@@ -7,22 +7,16 @@ class Main(Application):
     async def run(self):
 
         self.border = [0]
-        child_a = Widget(self).grid(0)
-        child_a.border = [0]
+        child_a = Widget(self, border=[0]).grid(0)
 
-        child_a_1 = Widget(child_a).grid(0, 0)
-        child_a_1.content = 'Label:'
-        spacer = Widget(child_a).grid(0, 1)
-        child_a_3 = Widget(child_a).grid(0, 2)
-        child_a_3.content = '<Button>'
-        child_a_4 = Widget(child_a).grid(1, 0).span(col=3).weight(2)
-        child_a_4.content = 'Details'
+        child_a_1 = Widget(child_a, 'Label:').grid(0, 0)
+        child_a_2 = Widget(child_a).grid(0, 1)  # spacer
+        child_a_3 = Widget(child_a, '<Button>').grid(0, 2)
+        child_a_4 = Widget(
+            child_a, 'Details', [0]).grid(1, 0).span(col=3).weight(3)
 
-        child_b = Widget(self).grid(1)
-        child_b.border = [0]
-        child_b.content = 'World'
-        child_c = Widget(self).grid(0, 1).span(2).weight(col=3)
-        child_c.border = [0]
+        child_b = Widget(self, 'World', [0]).grid(1)
+        child_c = Widget(self, border=[0]).grid(0, 1).span(2).weight(col=3)
 
 
 if __name__ == '__main__':
