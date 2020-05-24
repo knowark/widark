cac339cd591980a9fa1bbcecfa93cbab03cf3009
Esteban Echeverry <eecheverry@nubark.com>
2020-05-24 16:49:36 -0500

    Move version variables to widark __init__.py

--- tests/widget/__init__.py
+++
@@ -1,4 +0,0 @@
-
-__author__ = 'Knowark'
-__email__ = 'info@nubark.com'
-__version__ = '0.1.0'
--- widark/__init__.py
+++ widark/__init__.py
@@ -1,2 +1,7 @@
 from .application import Application
 from .widget import *
+
+
+__author__ = 'Knowark'
+__email__ = 'info@knowark.com'
+__version__ = '0.1.0'
